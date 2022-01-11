21b5406f11c5c9d77e21f3afa9e72c735de705a9
MRDenny <mrdenny@MRDennys-MBP.lan1>
2022-01-11 06:09:04 -0600

    navbar skeleton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,64 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Search } from "@material-ui/icons"
 
 const Container = styled.div`
   height: 60px;
-  background-color: black;
+`
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+`
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  
+  
+`
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  
+`
+
+const Center = styled.div`
+  flex: 1;
+  
+`
+
+const Right = styled.div`
+  flex:1;
+  
 `
 
 const Navbar = () => {
     return (
         <Container>
-            Navbar
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Search/>
+                        Search</SearchContainer>
+
+                </Left>
+                <Center>Center</Center>
+                <Right>Right</Right>
+
+            </Wrapper>
         </Container>
     )
 }
